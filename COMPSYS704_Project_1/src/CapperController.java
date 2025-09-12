922617cc5f99d22ff20fd8d638b76eb7d28d7b5a
apaik458 <albertminjunpaik@gmail.com>
2025-09-12 13:15:24 +1200

    Added files

---
+++ COMPSYS704_Project_1/src/CapperController.java
@@ -0,0 +1,97 @@
+import java.util.*;
+import com.systemj.ClockDomain;
+import com.systemj.Signal;
+import com.systemj.input_Channel;
+import com.systemj.output_Channel;
+
+public class CapperController extends ClockDomain{
+  public CapperController(String name){super(name);}
+  Vector currsigs = new Vector();
+  private boolean df = false;
+  private char [] active;
+  private char [] paused;
+  private char [] suspended;
+  public Signal testInput = new Signal("testInput", Signal.INPUT);
+  public Signal testOutput = new Signal("testOutput", Signal.OUTPUT);
+  private int S7 = 1;
+  
+  private int[] ends = new int[2];
+  private int[] tdone = new int[2];
+  
+  public void runClockDomain(){
+    for(int i=0;i<ends.length;i++){
+      ends[i] = 0;
+      tdone[i] = 0;
+    }
+    
+    RUN: while(true){
+      switch(S7){
+        case 0 : 
+          S7=0;
+          break RUN;
+        
+        case 1 : 
+          S7=2;
+          System.out.println("CapperController test");//sysj\CapperController.sysj line: 6, column: 2
+          S7=0;
+          active[1]=0;
+          ends[1]=0;
+          S7=0;
+          break RUN;
+        
+      }
+    }
+  }
+
+  public void init(){
+    char [] active1 = {1, 1};
+    char [] paused1 = {0, 0};
+    char [] suspended1 = {0, 0};
+    paused = paused1;
+    active = active1;
+    suspended = suspended1;
+    // Now instantiate all the local signals ONLY
+    // --------------------------------------------------
+  }
+  
+  public void run(){
+    while(active[1] != 0){
+      int index = 1;
+      if(paused[index]==1 || suspended[index]==1 || active[index] == 0){
+        for(int h=1;h<paused.length;++h){
+          paused[h]=0;
+        }
+      }
+      if(paused[1]!=0 || suspended[1]!=0 || active[1]!=1);
+      else{
+        if(!df){
+          testInput.gethook();
+          df = true;
+        }
+        runClockDomain();
+      }
+      testInput.setpreclear();
+      testOutput.setpreclear();
+      int dummyint = 0;
+      for(int qw=0;qw<currsigs.size();++qw){
+        dummyint = ((Signal)currsigs.elementAt(qw)).getStatus() ? ((Signal)currsigs.elementAt(qw)).setprepresent() : ((Signal)currsigs.elementAt(qw)).setpreclear();
+        ((Signal)currsigs.elementAt(qw)).setpreval(((Signal)currsigs.elementAt(qw)).getValue());
+      }
+      currsigs.removeAllElements();
+      dummyint = testInput.getStatus() ? testInput.setprepresent() : testInput.setpreclear();
+      testInput.setpreval(testInput.getValue());
+      testInput.setClear();
+      testOutput.sethook();
+      testOutput.setClear();
+      if(paused[1]!=0 || suspended[1]!=0 || active[1]!=1);
+      else{
+        testInput.gethook();
+      }
+      runFinisher();
+      if(active[1] == 0){
+      	this.terminated = true;
+      }
+      if(!threaded) break;
+    }
+  }
+}
